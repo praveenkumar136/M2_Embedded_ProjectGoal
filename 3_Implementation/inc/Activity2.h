7c2ea3ff6875265652665191622129293955032e
PRAVEEN KUMAR K S <68271765+praveenkumar136@users.noreply.github.com>
2021-12-01 14:14:29 +0530

    Create Activity2.h

---
+++ 3_Implementation/inc/Activity2.h
@@ -0,0 +1,14 @@
+#ifndef ACTIVITY2_H_INCLUDED
+#define ACTIVITY2_H_INCLUDED
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+void InitADC();
+
+uint16_t ReadADC(uint8_t ch);
+
+uint16_t activity2_GetADC(void);
+
+#endif // ACTIVITY2_H_INCLUDED
